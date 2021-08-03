c8ff76a2b51306828a9272b21cec7fd4a784eebe
KAIPASUBHASH <51924367+KAIPASUBHASH@users.noreply.github.com>
2021-08-03 12:53:11 +0530

    Add files via upload

---
+++ shapes.py
@@ -0,0 +1,8 @@
+import geometry.area
+import geometry.volume
+
+print("Area of rectangle=" , geometry.area.rectangle(7,9))
+print("Area of triangle=",geometry.area.triangle(9,5))
+
+print("Area of cube=",geometry.volume.cube(3))
+print("Area of cuboid=",geometry.volume.cuboid (3,4,8))    
